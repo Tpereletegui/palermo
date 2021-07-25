d8a3acdd306a453cf95f595dcc58a32d693d643a
mncolla <maxincolla@gmail.com>
2021-07-25 03:37:36 -0300

    modifico estilos de las faq section y corrijo navbar

--- public/js/navbarResponsive.js
+++ public/js/navbarResponsive.js
@@ -1,4 +1,5 @@
 let btn = document.querySelector('.checkbtn');
+let navState = document.querySelector('#check');
 let icon = document.querySelector('#icon');
 let navbar = document.querySelector('#navbar');
 let logo = document.querySelector('.logo');
@@ -8,8 +9,10 @@ let hiddenNavInput = document.querySelector('#hidden-input-nav');
 let locPath = window.location.pathname;
 let langIcon = document.querySelector('.lang-button');
 
+
+/* BOTÓN QUE ABRE EL NAV RESPONSIVE */
 btn.addEventListener("click", () =>{
-  if (icon.classList.contains('fa-bars')){
+  if (navState.checked==false){
     icon.classList.remove('fa-bars');
     icon.classList.add('fa-times');
     body.classList.add('no-scroll');
@@ -20,13 +23,13 @@ btn.addEventListener("click", () =>{
   }
 })
 
-
+/* MANTENER LA DIRECCIÓN AL CAMBIAR DE IDIOMA */
 langIcon.addEventListener('click',()=>{
   hiddenNavInput.setAttribute("value",locPath);
 })
 
 
-
+/* AJUSTE NAVBAR CUANDO HACEMOS SCROLL */
 if (window.innerWidth > 768){
   window.addEventListener('scroll', function () {
     if (window.pageYOffset > 0) {
@@ -37,17 +40,28 @@ if (window.innerWidth > 768){
       logo.innerHTML = '<img id="logo-img" src="/images/logos/logo-navbar-big.png" style="margin-left: 20px; margin-top:15px;"  alt="" width="100px" >'
     }
   });
+
 }else{
   navbar.style.height = "60px";
   logo.innerHTML = '<img id="logo-img" src="/images/logos/logo-navbar-landscape.png" alt="" width="160px">'
 }
 
 
+/* CUANDO HACEMOS CLICK EN ALGÚN LINK, QUE SE CIERRE EL NAV */
 if (window.innerWidth <= 768){
   links.forEach(link=>{
     link.addEventListener('click',()=>{
-      navbar.style.display = "none";
       body.classList.remove('no-scroll');
+      icon.classList.remove('fa-times');
+      icon.classList.add('fa-bars');
+      navState.checked = false;
     })
   })
+}
+
+
+
+/* AJUSTES RESPONSIVE HOME */
+if (window.innerWidth <=576){
+  document.querySelector('.faq-title-label').innerHTML='FAQ';
 }
